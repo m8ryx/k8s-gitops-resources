ba395aa84aab4d2cf77ed9268d12aa4c60aaefed
Rick Rezinas <rick@rezinas.com>
2021-03-27 10:00:10 -0700

    parameterized scripts and repointed to my repos

--- chapter-02/gitops-ci.sh
+++ chapter-02/gitops-ci.sh
@@ -1,13 +1,18 @@
 export VERSION=$(git rev-parse HEAD | cut -c1-7)
+export REPO_NAME='k8s-gitops-sample-app-deployment'
+export GIT_USER='m8ryx'
+export DOCKER_USER='m8ryx'
+export TARGET_APP='sample-app'
+
 make build
 make test
 
-export NEW_IMAGE="gitopsbook/sample-app:${VERSION}"
+export NEW_IMAGE="${DOCKER_USER}/${TARGET_APP}:${VERSION}"
 docker build -t ${NEW_IMAGE} .
 docker push ${NEW_IMAGE}
 
-git clone http://github.com/gitopsbook/sample-app-deployment.git
-cd sample-app-deployment
+git clone git@github.com:${GIT_USER}/${REPO_NAME}.git
+cd ${REPO_NAME}
 
 kubectl patch \
   --local \
@@ -17,11 +22,12 @@ kubectl patch \
         template:
           spec:
             containers:
-            - name: sample-app
+            - name: ${TARGET_APP}
               image: ${NEW_IMAGE}" \
   > /tmp/newdeployment.yaml
 mv /tmp/newdeployment.yaml deployment.yaml
 
-git commit deployment.yaml -m "Update sample-app image to ${NEW_IMAGE}"
+pwd
+git commit deployment.yaml -m "Update ${TARGET_APP} image to ${NEW_IMAGE}"
 git push
 
